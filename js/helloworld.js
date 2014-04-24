cb2d518a36a5f2e485e349fbef28874907d2be8b
NatsumiKanamori <ntm7.bkrp.l8@gmail.com>
2014-04-24 14:32:55 +0900

    hellonatsumiを追加

--- js/helloworld.js
+++ js/helloworld.js
@@ -5,3 +5,12 @@ var helloWorld = function(){
 
 var showHelloWorldButton = document.getElementById("showHelloWorld");
 showHelloWorldButton.addEventListener("click", helloWorld);
+
+var helloWorld = function(){
+	var message = document.getElementById("hellonatsumi");
+	message.setAttribute("class", "");
+};
+
+var showHelloWorldButton = document.getElementById("showHellonatsumi");
+showHelloWorldButton.addEventListener("click", helloWorld);
+
